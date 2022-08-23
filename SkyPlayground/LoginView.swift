f2cd73411930393f3a4f2296c69584bad520b015
sky-leonellima <leonel.lima@sky.uk>
2022-08-23 17:46:55 +0100

    enable and disable text fields

--- SkyPlayground/LoginView.swift
+++ SkyPlayground/LoginView.swift
@@ -1,35 +1,27 @@
 import SwiftUI
 
-enum FocusedField {
-    case email
-    case password
-}
-
 // - MARK: Modifiers
 
-extension View {
-    func conditionalModifier<M1, M2>(_ condition: Bool, _ truthyModifier: M1, falsyModifier: M2) -> some View where M1: ViewModifier, M2: ViewModifier {
-        Group {
-            if condition {
-                self.modifier(truthyModifier)
-            } else {
-                self.modifier(falsyModifier)
-            }
-        }
-    }
-}
-
 struct CTAButtonModifier: ViewModifier {
+    
+    @Environment(\.isEnabled)
+    private var isEnabled: Bool
+    
     func body(content: Content) -> some View {
         content
-            .font(.body)
+            .font(.system(size: 14, weight: .medium))
             .padding([.top, .bottom], 8)
             .padding([.leading, .trailing], 12)
             .frame(maxWidth: .infinity)
-            .foregroundColor(.white)
-            .background(.yellow)
-            .cornerRadius(8)
+            .foregroundColor(isEnabled ? .black : Color("disabled-text"))
+            .background(isEnabled ? Color("yellow") : Color("gray"))
+            .cornerRadius(4)
     }
+}
+
+enum FocusedField {
+    case email
+    case password
 }
 
 struct LoginTextInputModifier<T>: ViewModifier where T: Hashable {
@@ -46,13 +38,31 @@ struct LoginTextInputModifier<T>: ViewModifier where T: Hashable {
     func body(content: Content) -> some View {
         content
             .disabled(!isEnabled)
-            .font(.body)
+            .font(.system(size: 14, weight: .bold))
             .focused(focused, equals: focusedState)
             .padding(EdgeInsets(top: 12, leading: 7, bottom: 12, trailing: 7))
-            .border(.gray, width: 1)
-            .cornerRadius(8)
+            .overlay(RoundedRectangle(cornerRadius: 4,
+                                      style: .continuous)
+                .stroke(.gray, lineWidth: isEnabled ? 1 : 0))
     }
 }
+
+struct BorderedView: ViewModifier {
+    private let cornerRadius: CGFloat
+    private let width: CGFloat
+    
+    init(cornerRadius: CGFloat, width: CGFloat) {
+        self.cornerRadius = cornerRadius
+        self.width = width
+    }
+    func body(content: Content) -> some View {
+        content.overlay(RoundedRectangle(cornerRadius: cornerRadius,
+                                         style: .continuous)
+            .stroke(.gray, lineWidth: width))
+    }
+}
+
+// - MARK: Views
 
 struct LoginView: View {
     
@@ -76,19 +86,20 @@ struct LoginView: View {
                 VStack(alignment: .center, spacing: 24) {
                     VStack(alignment: .leading, spacing: 4) {
                         if canTypePassword {
-                            Text("e-mail")
+                            Text("Email")
                                 .font(.caption)
                         }
-                        TextField("E-mail", text: $email)
-                            .modifier(LoginTextInputModifier(isEnabled: canTypePassword,
+                        TextField("Email", text: $email)
+                            .modifier(LoginTextInputModifier(isEnabled: !canTypePassword,
                                                              focused: $focused,
                                                              focusedState: .email))
                     }
                     if canTypePassword {
                         VStack(alignment: .leading, spacing: 4) {
-                            Text("password")
+                            Text("Password")
                                 .font(.caption)
-                            TextField("password", text: $password)
+                            TextField("Password", text: $password)
+                                .textContentType(.password)
                                 .modifier(LoginTextInputModifier(isEnabled: true,
                                                                  focused: $focused,
                                                                  focusedState: .password))
@@ -96,9 +107,9 @@ struct LoginView: View {
                     }
                     
                     Button(action: onLoginPressed) {
-                        Text(verbatim: "Login")
+                        Text(verbatim: "Continue")
                             .modifier(CTAButtonModifier())
-                    }
+                    }.disabled(!hasValidEmail(email: email))
                 }
                 .padding([.leading, .trailing], 32)
                 .padding([.top, .bottom], 24)
@@ -111,16 +122,31 @@ struct LoginView: View {
         .onTapGesture(perform: removeFocus)
     }
     
-    private func onLoginPressed() { }
+    private func onLoginPressed() {
+        canTypePassword = true
+    }
     
     private func onCancelPressed() { }
     
     private func removeFocus() { self.focused = nil }
+    
+    private func hasValidEmail(email: String) -> Bool {
+        let emailPattern = #"^\S+@\S+\.\S+$"#
+        let result = email.range(
+            of: emailPattern,
+            options: .regularExpression
+        )
+        return (result != nil)
+    }
 }
+
+
+// - MARK: Preview
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+            .previewInterfaceOrientation(.portrait)
         //            .environmentObject(KeyboardVisibilityObserver())
     }
 }
